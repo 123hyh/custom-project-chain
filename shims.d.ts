1ce69c55666af290c926a1d14714177596449331
123hyh <310515005@qq.com>
2021-02-04 00:05:24 +0800

    添加菜单

--- shims.d.ts
+++ shims.d.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: huangyuhui
  * @Date: 2021-01-27 17:58:34
- * @LastEditors: huangyuhui
- * @LastEditTime: 2021-02-03 12:13:03
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-02-03 23:28:43
  * @Description:
  * @FilePath: \custom-project-chain\shims.d.ts
  */
@@ -18,5 +18,8 @@ declare module '*.module.scss' {
   const styles: { readonly [prop: string]: string }
   export default styles
 }
-
+declare module '*.json' {
+  const jsonData: { readonly [prop: string]: string } | Array<any>
+  export default jsonData
+}
 declare module '*.scss'
--- src/view/home/menu/index.tsx
+++ src/view/home/menu/index.tsx
@@ -1,15 +1,44 @@
 /*
  * @Author: huangyuhui
  * @Date: 2021-02-03 18:06:29
- * @LastEditors: huangyuhui
- * @LastEditTime: 2021-02-03 18:29:13
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-02-04 00:05:02
  * @Description: 菜单组件
  * @FilePath: \custom-project-chain\src\view\home\Menu\index.tsx
  */
 
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import { Menu } from 'ant-design-vue'
 import $style from '../index.module.scss'
+import menuOption from './tem.json'
+type MenuItemType = {
+  title: string
+  menuCode: string
+  children?: MenuItemType[]
+}
+
+const generateMenu = (menuItem: MenuItemType, prefixPath: string) => {
+  const currentPath = `${prefixPath}${menuItem.menuCode}`
+  return menuItem.children ? (
+    <Menu.SubMenu data-sub-path={currentPath} key={currentPath}>
+      {{
+        title: () => <>{menuItem.title}</>,
+        default: () =>
+          menuItem.children?.map(item => generateMenu(item, currentPath))
+      }}
+    </Menu.SubMenu>
+  ) : (
+    <Menu.Item data-path={currentPath} key={currentPath}>
+      {menuItem.title}
+    </Menu.Item>
+  )
+}
 
 export default defineComponent(() => {
-  return () => <menu class={$style.menu}>菜单</menu>
+  const menuState = reactive(menuOption)
+  return () => (
+    <Menu class={$style.menu} mode="inline">
+      {(menuState as MenuItemType[]).map(item => generateMenu(item, ''))}
+    </Menu>
+  )
 })
